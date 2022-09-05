3d229268300e6fa699766ce67c086df4765741c1
Noelia Alejandra Intriago Sánchez <noinsanc@espol.edu.ec>
2022-09-04 22:49:49 -0500

    fix on back

--- src/models/index.js
+++ src/models/index.js
@@ -6,7 +6,7 @@ const path = require('path');
 const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
     host: dbConfig.HOST,
     dialect: dbConfig.dialect,
-    operatorsAliases: false,
+    operatorsAliases: 0,
     pool: {
         max: dbConfig.pool.max,
         min: dbConfig.pool.min,
